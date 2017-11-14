7406b9e7527902e5b7d0e569ba8d31749ae405b1
angeloLuna <angel.luna.najera@gmail.com>
2017-11-13 21:54:52 -0600

    Pagina de American gods hecha con bootstrap

---
+++ js/script.js
@@ -0,0 +1,96 @@
+'use strict'
+// var number = 1
+
+// var float = .56823
+
+// var str = "hola"
+
+// var bool = true
+
+// var array = [1, 2, 3,4, "nel"]
+
+// var obj = {
+// 	nombre: "angelo",
+// 	apellido: "luna"
+// }
+
+// operadores logicos
+// == === != && || !
+
+// var jugador1 = "tijera"
+
+// var jugador2 = "papel"
+
+
+// if (2=="2") {
+// 	console.log(" ")
+// 	if (2 === "2") {
+// 		console.log()
+// 	}
+// } else if (true) {
+
+// } else if (true) {
+
+// }else{
+
+// }
+
+// console.log(jugador2)
+
+
+// var boleto = "comprado"
+// var mayorEdad = "si"
+
+// if (boleto == "comprado") {
+
+// 	// console.log("puedes entrar al cine")
+
+// 	if (mayorEdad == "si") {
+// 		console.log("diviertete")
+// 	} else{
+// 		console.log("vete a ver los minions")
+// 	}
+
+// } else{
+// 	console.log("saquese")
+// }
+
+
+var persona1 = "piedra"
+var persona2 = "papel"
+
+if (persona1 == "papel") {
+
+	if (persona2 == "piedra") {
+		console.log("persona1 gana")
+	} else if (persona2== "papel") {
+		console.log("empate")
+	} else{
+		console.log ("persona2 gana")
+	} 
+} else if (persona1 == "piedra"){
+	
+	if (persona2 == "piedra") {
+		console.log("empate")
+	} else if (persona2== "papel") {
+		console.log("persona 2 gana")
+	} else{
+		console.log ("persona1 gana")
+	} 
+} else if (persona1 == "tijeras"){
+	
+	if (persona2 == "piedra") {
+		console.log("persona2 gana")
+	} else if (persona2== "papel") {
+		console.log("persona 1 gana")
+	} else{
+		console.log ("empate")
+	}
+} 
+
+
+!=
+
+
+
+
