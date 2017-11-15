14c6c32191547fb8e729626f69c4d714dacda307
angeloLuna <angel.luna.najera@gmail.com>
2017-11-15 15:31:37 -0600

    conceptos básicos de Javascript

---
+++ javascript-basico/basico.js
@@ -0,0 +1,68 @@
+// Declaracion de variables
+// var numero = 2
+// var texto = "hola"
+
+// operadores
+// * + - / % 
+
+// operadores logicos
+// == igual
+// >= <=  mayor-igual menor-igual
+// !=  diferente
+
+// ------------------------------------
+// programa para saber si un numero es par o non
+// inputs
+// var numero = prompt("Escribe un numero")
+
+// var mod = numero % 2
+
+// if (mod != 0) {
+// 	console.log("impar")
+// } else{
+// 	console.log("par")
+// }
+// ---------------------------------------
+
+// reasignacion de valores a una variable mostrando en consola su valor en cada asignacion nueva
+// var x = 10
+// console.log("a", x )
+// // x += 20
+// x = x + 20
+// console.log("b", x )
+// x = x + 20
+// console.log("c", x )
+
+
+// ---------------------
+// reasignacion de valor a una variable
+// x = 10 
+
+// x = 6 * 8
+
+// x = x
+
+// x = "hola"
+
+// x = 0
+
+// console.log(x)
+// -------------------
+
+// Tipos de variables
+
+// var a = 10
+// var b = "hola"
+// var c = 1.5
+
+// var d = "gatitos"
+
+// Mostramos distintos elementos en un solo console.log("texto", variable, tipodevariable)
+// console.log( "la variable a es: ", a, typeof(a) )
+// console.log( "la variable b es: ", b, typeof(b) )
+// console.log( "la variable c es: ", c, typeof(c) )
+// console.log( "la variable d es: ", d, typeof(d) )
+// -----------------------
+
+
+
