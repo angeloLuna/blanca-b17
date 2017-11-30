ca6e856840ef0d48aa9681e261df3cb8e5969aac
angeloLuna <angel.luna.najera@gmail.com>
2017-11-29 20:30:34 -0600

    agregado bloque de opciones del jugador 2, y  funciones de control de seleccion de los jugadores en el piedra papel o tijeras

--- piedra/script.js
+++ piedra/script.js
@@ -1,9 +1,11 @@
 
 function selectJugador1(){
-	var seleccion = document.querySelector('input[name="jugador1"]:checked').value
+	var jugador1 = document.querySelector('input[name="jugador1"]:checked').value
+	console.log( "jugador1", jugador1)
 }
 
 
 function selectJugador2(){
-	
+	var jugador2 = document.querySelector('input[name="jugador2"]:checked').value
+	console.log( "jugador2", jugador2)
 }
